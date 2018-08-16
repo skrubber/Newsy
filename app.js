779334d193e3dca082a29fc2d4ee8ae200291109
Yashaswy Rao <21307@MIPL.COM>
2018-08-16 11:16:30 +0530

    Hyperlinks, CBRE as default on load and Download icon added.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
-var path = require('path');
-var JSZip = require('jszip');
-var fs = require('fs');
 var app = express();
+var fs = require('fs');
+var path = require('path');
+var LinkModule = require('docxtemplater-link-module');
+var linkModule = new LinkModule();
 var ImageModule = require('docxtemplater-image-module')
 var opts = {}
 opts.centered = false;
@@ -27,8 +28,8 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/jsfiles'));
 app.use(express.static(__dirname + '/csvfiles'));
 app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/images'));
 app.use(express.static(__dirname + '/documents'));
-
 app.get('/doc/:id', (req, res) => {
 
     try {
@@ -44,53 +45,92 @@ app.get('/doc/:id', (req, res) => {
         var investment_data = (companydata.filter((data) => data.ranked === '4'));
         var partnership_data = (companydata.filter((data) => data.ranked === '3'));
         var lookforward_data = (companydata.filter((data) => data.ranked === '2'));
-        var JSZip = require('jszip');
-        var Docxtemplater = require('docxtemplater');
-        var content = fs.readFileSync(path.resolve(__dirname, 'documents/input.docx'), 'binary');
-        var zip = new JSZip(content);
-        var doc = new Docxtemplater();
-        doc.attachModule(imageModule)
-        doc.loadZip(zip);
         var emptydata = {
             'industry': '-',
             'Company': '-',
+            'link': '-',
             'title': '-'
         }
-        if (companydata[0].rival == 'wework') { logo = 'images/WeWork-Logo_copy.jpg' }
-        else if (companydata[0].rival == 'cushman & wakefield') { logo: 'images/c&w.png' }
-        else if (companydata[0].rival == 'commercial real estate') { logo = 'images/CRE_logo_horizontal_RGB.png' }
-        else if (companydata[0].rival == 'jll') { logo = 'images/jll.jpg' }
-        else if (companydata[0].rival == 'savills') { logo = 'images/Savills_logo.svg+copy.jpg' }
-        else if (companydata[0].rival == 'eastdil secured') { logo = 'images/bgcsf_corp_partner_eastdil.jpg' }
-        else if (companydata[0].rival == 'marcus & millichap') { logo = 'images/Marcus-Millichap-Logo-promo.jpg' }
-        else if (companydata[0].rival == 'hff') { logo = 'images/HFF-logo.jpg' }
-        else if (companydata[0].rival == 'newmark group') { logo = 'images/newmark-group-inc-logo.png' }
-        else if (companydata[0].rival == 'colliers') { logo = 'images/colliers.jpeg' }
-        if (acquisitions_data.length == 0) { data = emptydata } else { data = acquisitions_data }
-        if (investment_data.length == 0) { data1 = emptydata } else { data1 = investment_data }
-        if (partnership_data.length == 0) { data2 = emptydata } else { data2 = partnership_data }
-        if (lookforward_data.length == 0) { data3 = emptydata } else { data3 = lookforward_data }
-        //set the templateVariables
-        doc.setData({
-            image: logo,
-            date: companydata[0].Dated,
-            heading: 'Competitor Deal Engagement Overview',
-            industry_name: req.params.id,
-            over_view_heading: 'Investment Themes Overview',
-            no_of_acquisitions: acquisitions_data.length,
-            no_of_investments: investment_data.length,
-            no_of_partnerships: partnership_data.length,
-            no_of_look_forwards: lookforward_data.length,
-            acquisition_table: data,
-            investment_table: data1,
-            partnership_table: data2,
-            lookforward_table: data3
-        });
-
-
-        doc.render();
-        var buf = doc.getZip().generate({ type: 'nodebuffer' });
-        fs.writeFileSync(path.resolve(__dirname, 'documents/' + `${req.params.id}` + '.docx'), buf);
+    //     var example = [{'link':''}];
+    //     for(var i=0; i < acquisitions_data.length; i++){
+    // example.push({'link':acquisitions_data[i].link})         }
+    // console.log(example)
+    //     var final = [];
+    //     for (var i = 0; i < acquisitions_data.length; i++) {
+    //          final[i] = (acquisitions_data[i].title).slice(0, 30);
+    //          acquisitions_data[i].newlink = final[i];
+    //     }
+    //     var final1 = [];
+    //     for (var i = 0; i < investment_data.length; i++) {
+    //          final1[i] = (investment_data[i].title).slice(0, 30);
+    //          investment_data[i].newlink = final1[i];
+    //     }
+    //     var final2 = [];
+    //     for (var i = 0; i < partnership_data.length; i++) {
+    //          final2[i] = (partnership_data[i].title).slice(0, 30);
+    //          partnership_data[i].newlink = final2[i];
+    //     }
+    //     var final3 = [];
+    //     for (var i = 0; i < lookforward_data.length; i++) {
+    //          final3[i] = (lookforward_data[i].title).slice(0, 30);
+    //          lookforward_data[i].newlink = final3[i];
+    //     }
+        
+        function sortOn(property){
+            return function(a, b){
+                if(a[property] < b[property]){
+                    return -1;
+                }else if(a[property] > b[property]){
+                    return 1;
+                }else{
+                    return 0;   
+                }
+            }
+        }
+        function mergeEquallyLabeledTypes(collector, type) {
+            var key = (type.industry); // identity key.
+            var store = collector.store;
+            var storedType = store[key];
+            if (storedType) {
+              storedType.Company = storedType.Company.concat("\n ",type.Company);
+              storedType.link = storedType.link.concat(", \n ",type.link);
+              storedType.title = storedType.title.concat(", \n ",type.title);
+            } else {
+              store[key] = type;
+              collector.list.push(type);
+            }
+            return collector;
+          }
+        acquisitions_data.sort(sortOn("industry")); // because `this.name = data.DepartmentName;`
+        if (acquisitions_data.length == 0) { data = emptydata } else { data = acquisitions_data.sort(sortOn("industry")); var datafor1 = data.reduce(mergeEquallyLabeledTypes, {  store:  {}, list:   [] }).list;}      
+        if (investment_data.length == 0) { data1 = emptydata } else { data1 = investment_data.sort(sortOn("industry")); var datafor2 = data1.reduce(mergeEquallyLabeledTypes, {  store:  {}, list:   [] }).list;}
+        if (partnership_data.length == 0) { data2 = emptydata } else { data2 = partnership_data.sort(sortOn("industry"));var datafor3 = data2.reduce(mergeEquallyLabeledTypes, {  store:  {}, list:   [] }).list;}
+        if (lookforward_data.length == 0) { data3 = emptydata } else { data3 = lookforward_data.sort(sortOn("industry"));   var datafor4 = data3.reduce(mergeEquallyLabeledTypes, {  store:  {}, list:   [] }).list;}
+        var DocxGen = require('docxtemplater');
+        var content = fs.readFileSync(__dirname + "/documents/input.docx", "binary");
+        var docx = new DocxGen()
+            .attachModule(linkModule)
+            .load(content)
+            .setData({
+                date: companydata[0].Dated,
+                heading: 'Competitor Deal Engagement Overview',
+                industry_name: req.params.id,
+                over_view_heading: 'Investment Themes Overview',
+                no_of_acquisitions: acquisitions_data.length,
+                no_of_investments: investment_data.length,
+                no_of_partnerships: partnership_data.length,
+                no_of_look_forwards: lookforward_data.length,
+                acquisition_table: datafor1,
+                investment_table: datafor2,
+                partnership_table: datafor3,
+                lookforward_table: datafor4,
+                 
+            }).
+            render();
+        var buffer = docx
+            .getZip()
+            .generate({ type: "nodebuffer" });
+        fs.writeFile(path.resolve(__dirname, 'documents/' + `${req.params.id}` + '.docx'), buffer);
     }
     catch (error) {
         var e = {
@@ -103,10 +143,8 @@ app.get('/doc/:id', (req, res) => {
         // The error thrown here contains additional information when logged with JSON.stringify (it contains a property object).
         throw error;
     }
-
     res.send({ 'CompanyID': req.params.id });
-})
-
+});
 app.listen(4000, () => {
     console.log("port is listening");
 })
--- jsfiles/main.js
+++ jsfiles/main.js
@@ -9,5 +9,18 @@ function opendoc() {
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send();
 }
-
-
+var obj1 = {
+    "name": "name1",
+    "fruit":"Orange",
+    "Link":"123"
+  };
+  var obj2 = {
+    "name": "name3",
+    "fruit":"Apple",
+    "Link":"xyz"
+  };
+  var obj3 = {
+    "name": "name1",
+    "fruit":"CUstard",
+    "Link":"abc"
+  };
